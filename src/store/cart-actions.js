93f05326999a51e2d870c259979a82a4ba348016
unknown <govind.bisen@gmail.com>
2022-02-19 16:10:38 +0530

    fetching the empty cart without error...uff tough one...

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -15,7 +15,12 @@ export const fetchCartData = () => {
     };
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -43,7 +48,10 @@ export const sendCartData = (cart) => {
         "https://cart-react-ece92-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
       if (!response.ok) {
